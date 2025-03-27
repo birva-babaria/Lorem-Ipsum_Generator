5ed9e0f7b4dc9acd6a321c041867d214c096eb8a
Birva Babaria <birvababaria@gmail.com>
2025-03-27 15:35:34 -0700

    Fetching from API

--- script.js
+++ script.js
@@ -8,10 +8,30 @@ let options = {
 };
 
 let url = "https://api.api-ninjas.com/v1/loremipsum?paragraphs=";
-let generateParas = () => {
-    let noOfParas = document.getElementById("paragraph-number")
+let generateParas = async () => {
+    let noOfParas = document.getElementById("paragraph-number").value
     console.log(noOfParas);
+
+    try{
+        let response = await fetch(url+noOfParas, options);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        let data = await response.json();
+        if (data){
+            console.log(data.text);
+            paraDisplay.innerText = data.text;
+        }
+    }
+    catch{
+        console.log("Error fetching data:", error);
+    }
 };
 
 generateBtn.addEventListener("click", generateParas);
 window.addEventListener("load", generateParas);
+
+copyBtn.addEventListener("click", () => {
+    navigator.clipboard.writeText(paraDisplay.innerText);
+    alert("Text Copied to Clipboard!");
+});
